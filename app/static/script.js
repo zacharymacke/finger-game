5d01cbbe287993c41ca2b71905ae5ba1a6059eac
John Jago <johnjago@protonmail.com>
2018-10-21 11:18:43 -0500

    Clean up

--- app/static/script.js
+++ app/static/script.js
@@ -1,87 +1,72 @@
-
 const video = document.querySelector('video');
 const reader = new FileReader();
-var videoTracks2;
-var picture;
+var webcam;
+var takePhotos;
+var randomNum;
 var counter = 0;
-var takePic;
-var r;
 
 const constraints = window.constraints = {
-	audio: false,
-	video: true
+  audio: false,
+  video: true
 };
 
+function sendPhoto() {
+  var request = new XMLHttpRequest();
+  request.open("POST", "http://localhost:5000/image", true);
+  request.onload = () => console.log("Uploaded")
 
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.responseText);
+      let fingerNumber = JSON.parse(request.responseText).fingerNumber;
+      if (fingerNumber === randomNum) {
+        generateRandom();
+      }
+    }
+  }
 
-function handleSuccess(stream){
-	videoTracks = stream.getVideoTracks();
-	videoTracks2 = stream.getVideoTracks()[0];
-	window.stream = stream;
-	video.srcObject = stream;
-};
+  webcam.takePhoto().then(blob => {
+    // Convert the image blob to Base64
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => request.send(reader.result)
+  });
 
-async function init(){
-	try{
-		const stream = await navigator.mediaDevices.getUserMedia(constraints);
-		handleSuccess(stream);
-	} catch (error){
-		console.log(error);
-	}
+  counter++;
+
+  if (counter === 15) {
+    // Stop taking/sending photos
+    clearInterval(takePhotos);
+    counter = 0;
+  }
 }
 
-function sendPhoto(){
-
-		counter++;
-		
-		var request = new XMLHttpRequest();
-		request.open("POST", "http://localhost:5000/image", true);
-		request.onload = function (oEvent){
-			console.log("uploaded");
-		};
-		
-		request.onreadystatechange = () => {
-			if(request.readyState === 4 && request.status === 200){
-				console.log(request.responseText);								
-				var fNum = JSON.parse(request.responseText).fingerNumber;
-				console.log(fNum);
-				if(fNum === r){
-					generateRandom();	
-				}
-				
-			}
-		}
-				
-		if(counter === 15){
-			clearInterval(takePic);
-			counter = 0;
-		}
-
-		picture.takePhoto()
-			.then(blob => {
-				reader.readAsDataURL(blob);
-				reader.onloadend = () => {
-					let base64 = reader.result;
-					request.send(base64);
-				}
-			});		
-};
-
-function generateRandom(){
-	r = Math.floor(Math.random() * 6);
-	document.getElementById("number").innerHTML = r;
-
+function generateRandom() {
+  randomNum = Math.floor(Math.random() * 6);
+  document.getElementById("number").innerHTML = randomNum;
 }
 
-function startGame(){
-	generateRandom();
+function startGame() {
+  generateRandom();
+  webcam = new ImageCapture(videoTracks[0]);
+  // Take and send photo every 2 seconds
+  takePhotos = setInterval(sendPhoto, 2000);
+}
 
-	picture = new ImageCapture(videoTracks2);
-	takePic = setInterval(sendPhoto, 2000)
-};
+function handleSuccess(stream) {
+  videoTracks = stream.getVideoTracks();
+  window.stream = stream;
+  // Display the webcam feed on the page
+  video.srcObject = stream;
+  document.getElementById("go").addEventListener("click", startGame);
+}
 
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 init();
-
-var el = document.getElementById("go");
-el.addEventListener("click", startGame)
